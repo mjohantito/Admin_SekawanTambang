dd090a8058a7c9ee587b8de202db39c2fb36c7c5
Manuel Johan Tito <mjohantito@student.ciputra.ac.id>
2024-07-16 22:41:13 +0700

    Initial Commit

---
+++ Admin_SekawanTambang/Admin_SekawanTambangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Admin_SekawanTambangApp.swift
+//  Admin_SekawanTambang
+//
+//  Created by Manuel Johan Tito on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Admin_SekawanTambangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
